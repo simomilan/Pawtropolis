cbc0fe03f47172a8a8a4154ac9c7eb4f56c53f7f
smilan <simomilan97@hotmail.it>
2023-06-20 15:34:41 +0200

    Modification of textGame, GoCommand and creation of InputController

---
+++ src/controller/InputController.java
@@ -0,0 +1,32 @@
+package controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputController {
+
+    public InputController(){
+
+    }
+
+    public static String readString() {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader inputReader = new BufferedReader(input);
+        try {
+            return inputReader.readLine();
+        } catch (IOException e) {
+            System.err.println("Error while reading user input");
+            return "";
+        }
+    }
+
+    public static boolean equalsIgnoreCase(String str1, String str2) {
+        return str1.equalsIgnoreCase(str2);
+    }
+
+    public static String cleanerInput(String input) {
+        return input.trim().toLowerCase();
+    }
+
+}

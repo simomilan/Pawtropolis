45705a4fd679a9b5977b1f3f0ce7c00d73e81aac
AlessandroTrupia <trupia.alessandro@libero.it>
2023-09-07 16:24:38 +0200

    Feat: add Spring Boot configuration in build.gradle and Main

--- app/src/main/java/pawtropolis/src/view/Main.java
+++ app/src/main/java/pawtropolis/src/view/Main.java
@@ -1,11 +1,13 @@
 package pawtropolis.src.view;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import pawtropolis.src.controller.TextGame;
-
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(Main.class,args);
         TextGame game = new TextGame();
         game.start();
 

e83b9a293fc63915a2d2b3c359ba5a46b8266542
simomilan <123069491+simomilan@users.noreply.github.com>
2023-09-27 17:09:13 +0200

    Refactor: shift main from view package to src

---
+++ app/src/main/java/pawtropolis/Main.java
@@ -0,0 +1,17 @@
+package pawtropolis;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pawtropolis.controller.TextGame;
+//@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+       // SpringApplication.run(Main.class,args);
+        TextGame game = new TextGame();
+        game.start();
+
+    }
+}
+
+

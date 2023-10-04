2a2952b1afb71dff7db9945c505710c0d803e473
AlessandroTrupia <trupia.alessandro@libero.it>
2023-10-04 14:52:55 +0200

    Refactor: remove import of BagView, add @Autowired

--- app/src/main/java/pawtropolis/command/DropCommand.java
+++ app/src/main/java/pawtropolis/command/DropCommand.java
@@ -1,10 +1,11 @@
 package pawtropolis.command;
 
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pawtropolis.controller.GameController;
 import pawtropolis.model.Item;
-import pawtropolis.view.BagView;
+
 @Component
 @Setter
 public class DropCommand implements GameCommand {
@@ -12,21 +13,21 @@ public class DropCommand implements GameCommand {
     private final GameController gameController;
     private  String itemName;
 
-    public DropCommand(GameController gameController) {
-        this.gameController = gameController;
+    @Autowired
+    public DropCommand(GameController gameController1) {
+        gameController = gameController1;
     }
 
     @Override
     public void execute() {
-        BagView bagView = new BagView();
         Item selectedItem = gameController.getPlayer().getItemInBag(itemName);
         if (selectedItem == null) {
-            bagView.displayItemNotFound(itemName);
+            gameController.getBagView().displayItemNotFound(itemName);
             return;
         }
         gameController.getPlayer().removeItemFromBag(selectedItem);
         gameController.getMapController().getCurrentRoom().addItem(selectedItem);
-        bagView.displayItemDropped(selectedItem.getName());
+        gameController.getBagView().displayItemDropped(selectedItem.getName());
     }
 
 }

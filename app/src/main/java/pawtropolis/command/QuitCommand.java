85bce977e5a568c819c407ae93dd8abc88acde57
AlessandroTrupia <trupia.alessandro@libero.it>
2023-10-04 15:26:24 +0200

    Refactor: remove import ConsoleView

--- app/src/main/java/pawtropolis/command/QuitCommand.java
+++ app/src/main/java/pawtropolis/command/QuitCommand.java
@@ -3,21 +3,20 @@ package pawtropolis.command;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pawtropolis.controller.GameController;
-import pawtropolis.view.ConsoleView;
+
 
 @Component
 public class QuitCommand implements GameCommand {
 
     GameController gameController;
-@Autowired
-    public QuitCommand(GameController gameController) {
-        this.gameController = gameController;
+    @Autowired
+    public QuitCommand(GameController gameControllerParam) {
+        gameController = gameControllerParam;
     }
 
     @Override
     public void execute() {
-        ConsoleView consoleView = new ConsoleView();
-        consoleView.displayQuitCommand();
+        gameController.getConsoleView().displayQuitCommand();
         gameController.endGame();
     }
 }

6911a614a302fa9b7dc2debf9f1b3f4fa7c41383
AlessandroTrupia <trupia.alessandro@libero.it>
2023-10-04 15:24:24 +0200

    Refactor: remove import TextGame, Bag, Player and use Spring

--- app/src/main/java/pawtropolis/Main.java
+++ app/src/main/java/pawtropolis/Main.java
@@ -2,18 +2,12 @@ package pawtropolis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import pawtropolis.controller.TextGame;
-import pawtropolis.model.Bag;
-import pawtropolis.model.Player;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
-        TextGame game = new TextGame(new Player(new Bag()));
-        game.start();
-
     }
 }
 

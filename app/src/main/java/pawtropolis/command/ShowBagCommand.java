3d24e68ffabbd83ba910a62fb9f33ac4c98e3194
AlessandroTrupia <trupia.alessandro@libero.it>
2023-10-04 16:08:52 +0200

    Refactor: remove import BagView, change constructor

--- app/src/main/java/pawtropolis/command/ShowBagCommand.java
+++ app/src/main/java/pawtropolis/command/ShowBagCommand.java
@@ -3,26 +3,25 @@ package pawtropolis.command;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pawtropolis.controller.GameController;
-import pawtropolis.view.BagView;
+
 
 @Component
 public class ShowBagCommand implements GameCommand {
 
     private final GameController gameController;
-@Autowired
-    public ShowBagCommand(GameController gameController) {
-        this.gameController = gameController;
+    @Autowired
+    public ShowBagCommand(GameController gameControllerParam) {
+        gameController = gameControllerParam;
     }
 
     @Override
     public void execute() {
-        BagView bagView = new BagView();
         String items = gameController.getPlayer().showBag();
         int availableSpace = gameController.getPlayer().getAvailableSpaceInBag();
         if (items.isEmpty()) {
-            bagView.displayEmptyBag();
+            gameController.getBagView().displayEmptyBag();
         } else {
-            bagView.displayBag(items, availableSpace);
+            gameController.getBagView().displayBag(items, availableSpace);
         }
     }
 }

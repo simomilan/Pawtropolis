4b8da75ba638328f0eec0b662d18421793ef6b77
AlessandroTrupia <trupia.alessandro@libero.it>
2023-10-05 13:11:14 +0200

    feat: add ViewFactory class

---
+++ app/src/main/java/pawtropolis/view/ViewFactory.java
@@ -0,0 +1,24 @@
+package pawtropolis.view;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public class ViewFactory {
+
+    private final BagView bagView;
+    private final ConsoleView consoleView;
+    private final DirectionView directionView;
+    private final RoomView roomView;
+
+    @Autowired
+    public ViewFactory(BagView bagView, ConsoleView consoleView, DirectionView directionView, RoomView roomView) {
+        this.bagView = bagView;
+        this.consoleView = consoleView;
+        this.directionView = directionView;
+        this.roomView = roomView;
+    }
+
+}

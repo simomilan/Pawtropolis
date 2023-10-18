ceab021f2ea68eb53fe41c664ac2536fe7ea9765
AlessandroTrupia <trupia.alessandro@libero.it>
2023-10-18 11:11:03 +0200

    feat: add tables bag,item,room, animal

--- script/schema.sql
+++ script/schema.sql
@@ -1,18 +1,43 @@
-CREATE TABLE item (
-                      id SERIAL PRIMARY KEY,
-                      name VARCHAR(255) NOT NULL,
-                      description TEXT,
-                      requiredSpace INT
+CREATE TABLE player (
+                        id SERIAL PRIMARY KEY,
+                        name VARCHAR(255) UNIQUE,
+                        lifePoints INT
 );
 
 CREATE TABLE bag (
                      id SERIAL PRIMARY KEY,
-                     availableSpace INT
+                     BAG_SPACE INT,
+                     id_player INT,
+                     FOREIGN KEY (id_player) references player(id)
 );
 
-CREATE TABLE player (
-                        id SERIAL PRIMARY KEY,
-                        name VARCHAR(255),
-                        lifePoints INT,
-                        bag_id BIGINT REFERENCES bag(id)
+;CREATE TABLE item (
+                       id SERIAL PRIMARY KEY,
+                       name VARCHAR(255) NOT NULL,
+                       description TEXT,
+                       requiredSpace INT,
+                       bag_id INT,
+                       FOREIGN KEY (bag_id) references bag(id)
+);
+
+CREATE TABLE room (
+                     id SERIAL PRIMARY KEY,
+                     name VARCHAR(255) NOT NULL,
+                     item_id INT,
+                     FOREIGN KEY (item_id) references item(id)
+);
+
+CREATE TABLE animal (
+                      id SERIAL PRIMARY KEY,
+                      nickname VARCHAR(255) NOT NULL,
+                      species VARCHAR(255) NOT NULL,
+                      favourite_food VARCHAR(255) NOT NULL,
+                      age INT,
+                      entry_date DATE,
+                      weight FLOAT,
+                      height FLOAT,
+                      wingspan FLOAT,
+                      tail_lenght FLOAT,
+                      room_id INT,
+                      FOREIGN KEY (room_id) references room(id)
 );

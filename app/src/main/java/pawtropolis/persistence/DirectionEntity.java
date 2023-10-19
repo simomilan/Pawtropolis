117732a7c03457b9750fef73a20591240db3a40d
AlessandroTrupia <trupia.alessandro@libero.it>
2023-10-19 12:37:39 +0200

    feat: (DirectionEntity) create class

---
+++ app/src/main/java/pawtropolis/persistence/DirectionEntity.java
@@ -0,0 +1,27 @@
+package pawtropolis.persistence;
+
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import pawtropolis.model.Direction;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "direction")
+public class DirectionEntity implements EntityDB{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "direction_id")
+    private Long directionID;
+
+    @Enumerated(EnumType.STRING)
+    private Direction direction;
+
+    @ManyToOne
+    @JoinColumn(name = "to_room_id")
+    private RoomEntity toRoom;
+}

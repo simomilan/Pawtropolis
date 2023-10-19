49fb845d495b4555b9d3c57385691285e0a33c0e
AlessandroTrupia <trupia.alessandro@libero.it>
2023-10-19 12:36:45 +0200

    refactor: (application.properties), (docker-compose.yml),(insertItem.sql)

--- script/insertItem.sql
+++ script/insertItem.sql
@@ -1,4 +1,5 @@
-INSERT INTO item(name, description,requiredSpace) VALUES ('collar','studded collar', 1),
+INSERT INTO item(name, description,requiredSpace) VALUES
+('collar','studded collar', 1),
 ('bone','a human femur', 3),
 ('whip','leather whip', 1),
 ('meat','sheep', 4),
